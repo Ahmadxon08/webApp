efa86aceb31d7388f4f162646f1920e3be61cc1b
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2024-12-05 15:59:14 +0500

    push

--- src/components/saveCart/SaveCart.jsx
+++ src/components/saveCart/SaveCart.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 import { useDrawerStore } from "../../store/useStore";
-
+import { useProducts } from "../../context/useContext";
+import "./SaveCart.scss";
+import Cart from "../cart/Cart";
 const SaveCart = () => {
+  const [totalPrice, setTotalPrice] = useState(0);
+  const { cartItem } = useProducts();
+
+  useEffect(() => {
+    const calculateTotalPrice = () => {
+      const price = cartItem.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+      );
+      setTotalPrice(price);
+    };
+
+    calculateTotalPrice();
+  }, [cartItem]);
   const { isDrawerOpen, closeDrawer } = useDrawerStore();
 
   const drawerContent = (
@@ -17,11 +33,37 @@ const SaveCart = () => {
         },
         padding: 2,
       }}
+      className="cartBody"
       role="presentation"
-      onClick={closeDrawer}
+      onClick={(e) => e.stopPropagation()}
       onKeyDown={closeDrawer}
       inert={isDrawerOpen ? null : "true"}
-    ></Box>
+    >
+      <h2>
+        Savatdagi tovarlar naxlari{" "}
+        <span>
+          {totalPrice.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </span>
+      </h2>
+
+      {cartItem.map((item, i) => (
+        <div className="cartItem" key={i}>
+          <div className="img_container">
+            <img
+              src={item.image}
+              alt=""
+              width={70}
+              height={50}
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <h4>{item.quantity}</h4>
+        </div>
+      ))}
+    </Box>
   );
 
   return (
